50ead95518c7f1f2770d416f04fd1cdce03ec702
Kyungjin <gulmok1217@hanyang.ac.kr>
2024-12-30 20:09:55 +0900

    Fix : Typography 단위 px로 변환

--- src/styles/Typography.jsx
+++ src/styles/Typography.jsx
@@ -1,97 +1,97 @@
 import styled from 'styled-components';
 
 export const H1 = styled.h1`
-  font-size: 32pt;
+  font-size: 32px;
   line-height: 150%;
   font-weight: 600;
 `;
 
 export const H2 = styled.h2`
-  font-size: 28pt;
+  font-size: 28px;
   line-height: 156%;
   font-weight: 600;
 `;
 
 export const H3 = styled.h3`
-  font-size: 24pt;
+  font-size: 24px;
   line-height: 150%;
   font-weight: 600;
 `;
 
 export const H4 = styled.h4`
-  font-size: 20pt;
+  font-size: 20px;
   line-height: 140%;
   font-weight: 600;
 `;
 
 export const H5 = styled.h5`
-  font-size: 16pt;
+  font-size: 16px;
   line-height: 150%;
   font-weight: 600;
 `;
 
 export const L600 = styled.p`
-  font-size: 20pt;
+  font-size: 20px;
   line-height: 150%;
   font-weight: 600;
 `;
 
 export const L500 = styled.p`
-  font-size: 20pt;
+  font-size: 20px;
   line-height: 140%;
   font-weight: 650;
 `;
 
 export const M600 = styled.p`
-  font-size: 16pt;
+  font-size: 16px;
   line-height: 150%;
   font-weight: 600;
 `;
 
 export const M500 = styled.p`
-  font-size: 16pt;
+  font-size: 16px;
   line-height: 150%;
   font-weight: 500;
 `;
 
 export const M400 = styled.p`
-  font-size: 16pt;
+  font-size: 16px;
   line-height: 150%;
   font-weight: 400;
 `;
 
 export const R600 = styled.p`
-  font-size: 14pt;
+  font-size: 14px;
   line-height: 168%;
   font-weight: 600;
 `;
 
 export const R500 = styled.p`
-  font-size: 14pt;
+  font-size: 14px;
   line-height: 168%;
   font-weight: 500;
 `;
 
 export const R400 = styled.p`
-  font-size: 14pt;
+  font-size: 14px;
   line-height: 168%;
   font-weight: 400;
 `;
 
 export const S500 = styled.p`
-  font-size: 12pt;
+  font-size: 12px;
   line-height: 132%;
   font-weight: 500;
 `; 
 
 export const S400 = styled.p`
-  font-size: 12pt;
+  font-size: 12px;
   line-height: 132%;
   font-weight: 400;
 `; 
 
 export const S300 = styled.p`
-  font-size: 12pt;
+  font-size: 12px;
   line-height: 132%;
   font-weight: 300;
 `; 
